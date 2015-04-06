656762037d9c6801f72ed2a14d102d6cc6413403
Ryan Schmitt <rschmitt@pobox.com>
2015-04-05 22:02:23 -0700

    Shell out to git instead of using libgit2

--- build.rs
+++ build.rs
@@ -1,12 +1,11 @@
 extern crate time;
-extern crate git2;
 
 use std::env;
 use std::fs::File;
 use std::io::Write;
 use std::path::Path;
 use time::{strftime, now};
-use git2::Repository;
+use std::process::Command;
 
 fn main() {
     let basedir = env::var("CARGO_MANIFEST_DIR").unwrap();
@@ -15,7 +14,7 @@ fn main() {
     let mut f = File::create(&dest_path).unwrap();
 
     let timestamp = strftime("%F %H:%M:%S %z", &now()).unwrap();
-    let commit = get_head_commit(&basedir);
+    let commit = get_head_commit();
 
     let contents = format!(
 "pub const VERSION: &'static str = \"{}\";
@@ -25,11 +24,9 @@ pub const COMMIT: &'static str = \"{}\";\n",
     f.write_all(contents.as_bytes()).unwrap();
 }
 
-fn get_head_commit(pwd: &str) -> String {
-    let repo = Repository::open(pwd).unwrap();
-    let revspec = repo.revparse(&"HEAD").unwrap();
-    let commit_id: git2::Oid = revspec.from().unwrap().id();
-    let full_id = commit_id.to_string();
-    let short_id = &full_id[0..8];
-    short_id.to_string()
+fn get_head_commit() -> String {
+    let output = Command::new("git").args(&["rev-parse", "--short", "HEAD"]).output().unwrap();
+    let mut rev = String::from_utf8(output.stdout).unwrap();
+    rev.pop();
+    rev
 }
