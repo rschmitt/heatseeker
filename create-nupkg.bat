4e891dfb9028b24cbafe9457699b49561a53e6f7
Ryan Schmitt <rschmitt@pobox.com>
2015-06-16 11:59:28 -0700

    Tweak create-nupkg.bat

--- create-nupkg.bat
+++ create-nupkg.bat
@@ -1,17 +1,17 @@
 @echo on
 
-cargo clean
-cargo test
-cargo build --release
+cargo clean || exit /b 1
+cargo test || exit /b 1
+cargo build --release || exit /b 1
 
-mkdir chocolatey
-cp heatseeker.nuspec chocolatey\
+mkdir chocolatey || exit /b 1
+copy heatseeker.nuspec chocolatey\ || exit /b 1
 
-mkdir chocolatey\tools
-cp LICENSE chocolatey\tools\
-cp README.md chocolatey\tools\README
+mkdir chocolatey\tools || exit /b 1
+copy LICENSE chocolatey\tools\ || exit /b 1
+copy README.md chocolatey\tools\README || exit /b 1
 
-mkdir chocolatey\tools\bin
-cp target\release\hs.exe chocolatey\tools\bin
+mkdir chocolatey\tools\bin || exit /b 1
+copy target\release\hs.exe chocolatey\tools\bin || exit /b 1
 
-cpack chocolatey\heatseeker.nuspec
+cpack chocolatey\heatseeker.nuspec || exit /b 1
