1c58b13652d5bbe6bf4457b535f830c6098c11cb
Ryan Schmitt <rschmitt@pobox.com>
2018-07-28 11:42:10 -0700

    Add VERIFICATION.txt

--- create-nupkg.bat
+++ create-nupkg.bat
@@ -1,17 +1,19 @@
 @echo on
 
-cargo test || exit /b 1
-cargo build --release || exit /b 1
-
 rd /s /q chocolatey
 mkdir chocolatey || exit /b 1
 copy heatseeker.nuspec chocolatey\ || exit /b 1
 
 mkdir chocolatey\tools || exit /b 1
 copy LICENSE chocolatey\tools\ || exit /b 1
+copy VERIFICATION.txt chocolatey\tools\ || exit /b 1
 copy README.md chocolatey\tools\README || exit /b 1
 
 mkdir chocolatey\tools\bin || exit /b 1
-copy target\release\hs.exe chocolatey\tools\bin || exit /b 1
+
+wget -q https://github.com/rschmitt/heatseeker/releases/download/v1.5.2/heatseeker-v1.5.2-x86_64-pc-windows-msvc.zip || exit /b 1
+7z x heatseeker-v1.5.2-x86_64-pc-windows-msvc.zip || exit /b 1
+del heatseeker-v1.5.2-x86_64-pc-windows-msvc.zip
+move hs.exe chocolatey\tools\bin || exit /b 1
 
 choco pack chocolatey\heatseeker.nuspec || exit /b 1
