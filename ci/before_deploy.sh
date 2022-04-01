9f9127c9607ac642de94a924d5aa1cdadad7e539
Ryan Schmitt <rschmitt@pobox.com>
2022-04-01 15:06:48 -0700

    CI: Remove `-C lto` flag

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -17,7 +17,7 @@ main() {
 
     test -f Cargo.lock || cargo generate-lockfile
 
-    cross rustc --bin hs --target $TARGET --release -- -C lto
+    cross rustc --bin hs --target $TARGET --release
     cp target/$TARGET/release/hs* $stage/
 
     cd $stage
